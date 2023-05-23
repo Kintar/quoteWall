c3f5df48e05bada4fc69eb905213d22b8cdd780e
Alec Lanter <kintar1900@gmail.com>
2023-05-22 20:22:28 -0500

    chore: initial commit

---
+++ src/component/QuoteLine.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let quote;
+    export let quoter;
+</script>
+
+<div>
+    {quote}
+    <p style="float: right">{quoter}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import QuoteLine from '../component/QuoteLine.svelte'
+</script>
+
+<h1>Quote</h1>
+
+<QuoteLine quote={"This is a quote."} quoter={"unknown"} />
